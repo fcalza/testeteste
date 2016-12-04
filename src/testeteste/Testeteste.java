aecce3715fce865a45ca58d708bea897652bd098
fcalza <notelipe@DESKTOP-6FLFT4S>
2016-12-04 19:31:15 -0200

    testeteste para diferenças

--- src/testeteste/Testeteste.java
+++ src/testeteste/Testeteste.java
@@ -11,11 +11,16 @@ package testeteste;
  */
 public class Testeteste {
 
+    private static Testeteste testando;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //teste diferença
+        testando = new Testeteste();
+        
     }
     
 }
